3e858891e73c375328badd8a2fb5c9318621a38e
Kirtesh Wani <kirtesh.wani@ideas.com>
2023-04-14 15:24:15 +0530

    adding logs

--- src/main/java/com/ideas/anymocker/core/components/ProcessorClosureWrapper.java
+++ src/main/java/com/ideas/anymocker/core/components/ProcessorClosureWrapper.java
@@ -6,25 +6,30 @@ import org.codehaus.groovy.runtime.MethodClosure;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 public class ProcessorClosureWrapper implements RequestProcessor {
+    private final Logger log;
     private final MethodClosure pre;
     private final MethodClosure process;
     private final MethodClosure post;
     private final MethodClosure init;
 
+
     private final MethodClosure download;
 
-    public ProcessorClosureWrapper(MethodClosure pre, MethodClosure process, MethodClosure post,MethodClosure init,MethodClosure download) {
+    public ProcessorClosureWrapper(Logger log, MethodClosure pre, MethodClosure process, MethodClosure post, MethodClosure init, MethodClosure download) {
+        this.log = log;
         this.pre = pre;
         this.process = process;
         this.post = post;
         this.init = init;
         this.download = download;
+
     }
 
     public void init(List<Request> requests) {
-        this.init.call(requests);
+        this.init.call(log,requests);
     }
 
     @Override
@@ -34,21 +39,21 @@ public class ProcessorClosureWrapper implements RequestProcessor {
 
     @Override
     public ResponseEntity<String> process(Request match, String body, HttpServletRequest req) {
-        return (ResponseEntity<String>) process.call(match,body,req);
+        return (ResponseEntity<String>) process.call(log,match,body,req);
     }
 
     @Override
     public void postProcess(Request match, String body, HttpServletRequest req) {
-        post.call(match, body, req);
+        post.call(log,match, body, req);
     }
 
     @Override
     public void preProcess(Request match, String body, HttpServletRequest req) {
-        pre.call(match,body,req);
+        pre.call(log,match,body,req);
     }
 
     @Override
     public void downloadFixtures(Request match) {
-        download.call(match);
+        download.call(log,match);
     }
 }
