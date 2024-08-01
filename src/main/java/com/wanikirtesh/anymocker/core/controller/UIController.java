2bf36c4d22442309a08f0d0692acd26857003af6
Kirtesh Wani <kirtesh.wani@ideas.com>
2024-08-01 11:26:05 +0530

    set grouping on config

--- src/main/java/com/wanikirtesh/anymocker/core/controller/UIController.java
+++ src/main/java/com/wanikirtesh/anymocker/core/controller/UIController.java
@@ -3,16 +3,19 @@ package com.wanikirtesh.anymocker.core.controller;
 import com.wanikirtesh.anymocker.core.components.Request;
 import com.wanikirtesh.anymocker.core.service.RequestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Controller
 public class UIController {
+    @Value("${requests.grouping}")
+    Boolean groupRequests;
+
     @Autowired
     private RequestFactory requestFactory;
     @RequestMapping(method = RequestMethod.GET, path = "/MOCKER/MANAGE")
@@ -24,25 +27,25 @@ public class UIController {
 
     private Map<String, Map<String, Map<String,List<Request>>>> groupRequests(List<Request> requestList) {
         Map<String, Map<String, Map<String,List<Request>>>> resultMap = new HashMap<>();
-        for (Request request : requestList) {
-            String[] parts = request.getUrl().split("/");
-            if(parts.length>=4){
-                String product = parts[1];
-                String module = parts[2];
-                String persona = parts[3];
-                resultMap
-                        .computeIfAbsent(product, k -> new HashMap<>())
-                        .computeIfAbsent(module, k -> new HashMap<>())
-                        .computeIfAbsent(persona, k -> new ArrayList<>())
-                        .add(request);
-            }else{
-                resultMap
-                        .computeIfAbsent("PRODUCT", k -> new HashMap<>())
-                        .computeIfAbsent("MODULE", k -> new HashMap<>())
-                        .computeIfAbsent("PERSONA", k -> new ArrayList<>())
-                        .add(request);
+            for (Request request : requestList) {
+                String[] parts = request.getUrl().split("/");
+                if (4 <= parts.length && groupRequests) {
+                    String product = parts[1];
+                    String module = parts[2];
+                    String persona = parts[3];
+                    resultMap
+                            .computeIfAbsent(product, k -> new HashMap<>())
+                            .computeIfAbsent(module, k -> new HashMap<>())
+                            .computeIfAbsent(persona, k -> new ArrayList<>())
+                            .add(request);
+                } else {
+                    resultMap
+                            .computeIfAbsent("PRODUCT", k -> new HashMap<>())
+                            .computeIfAbsent("MODULE", k -> new HashMap<>())
+                            .computeIfAbsent("PERSONA", k -> new ArrayList<>())
+                            .add(request);
+                }
             }
-        }
         return resultMap;
     }
 
